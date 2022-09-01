2b828e92519c62bdd852014ad9e5abc187c79e54
Jacek Gajek <jacek.gajek@codibly.com>
2022-09-01 11:16:01 +0200

    gauss_elimination.ipynb

--- print_utils.py
+++ print_utils.py
@@ -1,6 +1,7 @@
 import IPython.display as dis
 import numpy as np
 
+
 def printmd(expr):
     dis.display(dis.Markdown(r"$$\begin{align}" + expr + r"\end{align}$$"))
 
@@ -24,3 +25,13 @@ def printpol(p):
 
     expr = [op(a) + powstr(k) for k, a in enumerate(p[0:len(p)])]
     printmd("s(z)=" + ''.join(np.flip(expr)))
+
+
+def printmatrix(A: np.ndarray):
+    expr = r"\begin{bmatrix}"
+    n, m = A.shape
+    for i in range(0, n):
+        # for j in range(0, m):
+        expr += "&".join([str(A[i, j]) for j in range(m)]) + r"\\"
+    expr += "\end{bmatrix}"
+    return expr
